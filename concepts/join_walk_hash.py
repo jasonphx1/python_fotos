f70363526b4698235305a4a387f6c0a479613f5c
jasonphx1 <jasonphx1@gmail.com>
2018-11-20 23:06:50 -0700

    Adding full path name to code, should allow recursive searches.

--- concepts/join_walk_hash.py
+++ concepts/join_walk_hash.py
@@ -8,17 +8,24 @@ Created on Tue Nov 20 19:01:40 2018
 
 import os
 import hashlib
+import re
 
 hasher = hashlib.md5()
+txtCount = 0
+jpgCount = 0
+docCount = 0
 
 for root, dirs, files in os.walk(".", topdown=False):
     #Prints files
     for name in files:
-        print(os.path.join(root, name))
-        f=open(name, "rb")
+        fullName =os.path.join(root, name)
+        f=open(fullName, "rb")
         content = f.read()
         hasher.update(content)
         print(hasher.hexdigest())
+        if re.search('.txt', name):
+            txtCount += 1
+print("Number of text files: ", txtCount)
     #Prints directories
    # for name in dirs:
    #     print(os.path.join(root, name))
